53aa6f51fcf662e36a9550726b6119e6f3eec2c7
SAMI THAKUR <2022.sami.thakur@ves.ac.in>
2024-06-07 11:23:23 +0530

    final

--- src/widgets/About/info.jsx
+++ src/widgets/About/info.jsx
@@ -6,27 +6,16 @@ export default function Info() {
           <div className=" relative  lg:max-w-xl">
             <div className="lg:pr-5 relative z-40">
               <p className=" text-[14px] sm:text-[17px] text-[#404258] dark:text-[#b5b5b5] font-robo tracking-wide mt-5">
-                Hello, my name is Sami I am a second-year engineering student at
-                VESIT (Mumbai), majoring in Artificial Intelligence and Data
-                Science. I've been coding for about a year having started with
-                JAVA and HTML and made some static html sites. Fast forward
-                today I have a strong hand on core JAVA. Currently my main focus
-                is learning FULL STACK WEB DEVELOPMENT and DSA in JAVA and in
-                the future I aspire to learn about MACHINE LEARNING & DATA
-                SCIENCE
+                Hello, my name is Sami I am a second-year engineering student at VESIT (Mumbai), majoring in Artificial Intelligence and Data Science.
+                I've been coding for about a year having started with JAVA and HTML and made some static html sites. Fast forward today I have a
+                strong hand on core JAVA. Currently my main focus is learning FULL STACK WEB DEVELOPMENT and DSA in JAVA and in the future I aspire to
+                learn about MACHINE LEARNING & DATA SCIENCE
               </p>
-              <button
-                className="w-fit   bg-htext dark:bg-dhtext dark:text-darkbg text-background text-[15px] sm:text-[18px]  p-4 py-3  mt-5 font-robo border-2 border-[#f8f8f8] dark:border-[#333333] hover:shadow-lg hover:shadow-gray-700  transition duration-500"
-                id="CV"
-                // onClick="window.open('./img/RESUME.pdf')"
-              >
-                Check My Resume
-              </button>
             </div>
           </div>
           <div className="sm:ml-10 lg:ml-32 lg:block lg:w-1/2 flex items-center ">
             <div className="justify-center flex">
-              <img src="src\assets\profile-pic.png" width={350} height={350} />
+              <img src="./public/assets/profile-pic.png" width={350} height={350} />
             </div>
           </div>
         </div>
--- src/widgets/Projects/Project_Detail.jsx
+++ src/widgets/Projects/Project_Detail.jsx
@@ -5,28 +5,19 @@ import { Icon } from "@iconify/react";
 export default function ProductDetail() {
   const { id, pid } = useParams();
   const project = projects.find((project) => project.id === id);
-  const content = project.content.find(
-    (content) => content.pid === pid,
-  );
+  const content = project.content.find((content) => content.pid === pid);
 
   return (
     <section className=" pl-10 sm:pl-20 lg:pl-32 py-36 pb-10 dark:bg-darkbg bg-background pr-10">
       <div class="max-w-xl mx-auto">
         <NavLink to="/project">
           <div class="flex  gap-1 mb-6 align-middle cursor-pointer">
-            <Icon
-              icon="ion:caret-back"
-              className="text-base text-[#404258] dark:text-[#b5b5b5] flex align-middle my-auto"
-            />
-            <h1 class="font-mono  tracking-wide text-[18px]   max-w-sm text-[#404258] dark:text-[#b5b5b5]">
-              Projects
-            </h1>
+            <Icon icon="ion:caret-back" className="text-base text-[#404258] dark:text-[#b5b5b5] flex align-middle my-auto" />
+            <h1 class="font-mono  tracking-wide text-[18px]   max-w-sm text-[#404258] dark:text-[#b5b5b5]">Projects</h1>
           </div>
         </NavLink>
         <div class="flex items-start justify-between mb-6 align-middle">
-          <h1 class="font-staat  tracking-wide- md:text-4xl text-2xl mt-1 sm:mt-0   max-w-sm dark:text-dhtext text-htext">
-            {content.name}
-          </h1>
+          <h1 class="font-staat  tracking-wide- md:text-4xl text-2xl mt-1 sm:mt-0   max-w-sm dark:text-dhtext text-htext">{content.name}</h1>
 
           <div className="flex flex-wrap mt-2 align-middle">
             {content.link && (
@@ -35,10 +26,7 @@ export default function ProductDetail() {
                 target="_blank"
                 class="dark:bg-primary-bg bg-secondary-bg dark:text-white text-zinc-700 border border-transparent rounded-md px-3 py-2 cursor-pointer hover:border-htext dark:hover:border-dhtext  transition duration-200"
               >
-                <Icon
-                  icon="akar-icons:link-out"
-                  className="text-xl sm:text-2xl text-htext dark:text-dhtext"
-                />
+                <Icon icon="akar-icons:link-out" className="text-xl sm:text-2xl text-htext dark:text-dhtext" />
               </a>
             )}
             {content.github && (
@@ -47,10 +35,7 @@ export default function ProductDetail() {
                 target="_blank"
                 class="dark:bg-primary-bg bg-secondary-bg dark:text-white text-zinc-700 border border-transparent rounded-md px-3 py-2 cursor-pointer hover:border-htext dark:hover:border-dhtext  transition duration-200"
               >
-                <Icon
-                  icon="bi:github"
-                  className="text-xl sm:text-2xl text-htext dark:text-dhtext"
-                />
+                <Icon icon="bi:github" className="text-xl sm:text-2xl text-htext dark:text-dhtext" />
               </a>
             )}
           </div>
@@ -59,13 +44,11 @@ export default function ProductDetail() {
         <figure className="max-w-lg flex items-center justify-center">
           <img
             className="h-auto max-w-full  border-[1px] dark:border-dhtext border-htext mt-1 p-2 object-cover "
-            src={`/src/assets/${content.image}`}
+            src={`/public/assets/${content.image}`}
           ></img>
         </figure>
         <div className="mt-6 dark:text-zinc-400 text-zinc-600 leading-relaxed">
-          <div className="font-staat font-semibold text-htext dark:text-dhtext text-[30px] tracking-wider">
-            Tech Stack
-          </div>
+          <div className="font-staat font-semibold text-htext dark:text-dhtext text-[30px] tracking-wider">Tech Stack</div>
           <div className="flex flex-wrap mb-4 -ml-3">
             {content.tech_stack.map((tech, index) => {
               return (
@@ -75,23 +58,14 @@ export default function ProductDetail() {
                   key={index}
                   class="dark:bg-primary-bg bg-secondary-bg border border-transparent rounded-md px-3 py-2 cursor-pointer hover:border-htext dark:hover:border-dhtext hover:-translate-y-0.5 transition duration-200"
                 >
-                  <Icon
-                    icon={tech.icon}
-                    className="text-[25px] text-[#404258] dark:text-[#b5b5b5] hover:text-htext dark:hover:text-dhtext"
-                  />
+                  <Icon icon={tech.icon} className="text-[25px] text-[#404258] dark:text-[#b5b5b5] hover:text-htext dark:hover:text-dhtext" />
                 </a>
               );
             })}
           </div>
-          <div className="font-staat font-semibold text-htext dark:text-dhtext text-[30px] tracking-wider">
-            Overview
-          </div>
-          <p className="mt-2 mb-6 font-robo text-[12px] sm:text-[15px] tracking-wide text-[#404258] dark:text-[#b5b5b5]">
-            {content.desc1}
-          </p>
-          <p className="mt-2 mb-6 font-robo text-[12px] sm:text-[15px] tracking-wide text-[#404258] dark:text-[#b5b5b5]">
-            {content.desc2}
-          </p>
+          <div className="font-staat font-semibold text-htext dark:text-dhtext text-[30px] tracking-wider">Overview</div>
+          <p className="mt-2 mb-6 font-robo text-[12px] sm:text-[15px] tracking-wide text-[#404258] dark:text-[#b5b5b5]">{content.desc1}</p>
+          <p className="mt-2 mb-6 font-robo text-[12px] sm:text-[15px] tracking-wide text-[#404258] dark:text-[#b5b5b5]">{content.desc2}</p>
         </div>
       </div>
     </section>
